5525ccb39459cca5a5f456a20c9904943871dcf7
Linden Crandall <slayer00911@gmail.com>
2023-02-01 08:43:16 +0900

    page updates

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LCWebApp.Controllers
 {
-    public class MusicController : Controller
+    public class ContactController : Controller
     {
         public IActionResult Index()
         {
---
+++ Controllers/PrivacyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LCWebApp.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
